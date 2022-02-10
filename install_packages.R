4842f6ee653711134593611753845778c831473c
Myo Minn Oo <myominnoo@users.noreply.github.com>
2022-02-10 12:53:49 +1000

    build baser v4.1.2 maintainer

--- install_packages.R
+++
@@ -1,8 +0,0 @@
-if (!requireNamespace("remotes")) install.packages("remotes")
-
-# Minimal install without vignettes
-remotes::install_github(
-    "ropensci/ruODK@main", 
-    dependencies = TRUE, 
-    upgrade = "ask",
-    build_vignettes = FALSE)
