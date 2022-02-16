55f0f1870e7293cf852a94ef9a368bd561299eee
myominnoo <dr.myominnoo@gmail.com>
2022-02-16 10:15:09 +1000

    build malcon dash 0.2 mapview

--- install_packages.R
+++ install_packages.R
@@ -1,13 +1,16 @@
-# installing package imports packages
-pkg_list <- c("mapview", "sf", "leafpop", "rgeos", "rnaturalearth")
-
-install.packages(pkgs = pkg_list, repos = "https://cran.rstudio.com/")
 
 install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
+devtools::install_github("r-spatial/mapview")
 
-for(i in c(pkg_list,"rnaturalearthhires")){
-    
-    if(!i %in% rownames(installed.packages())){
-        stop(paste("Package", i, "is not available"))
-    }
-}
+
+# # installing package imports packages
+# pkg_list <- c("mapview", "sf", "leafpop", "rgeos", "rnaturalearth")
+# 
+# install.packages(pkgs = pkg_list, repos = "https://cran.rstudio.com/")
+# 
+# for(i in c(pkg_list,"rnaturalearthhires")){
+#     
+#     if(!i %in% rownames(installed.packages())){
+#         stop(paste("Package", i, "is not available"))
+#     }
+# }
